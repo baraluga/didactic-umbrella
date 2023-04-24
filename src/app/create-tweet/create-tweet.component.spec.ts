b94bde486c5db8ad0f748a4c86aa4085f9225113
Brian Alexander <brian.peralta@engie.com>
2023-04-24 15:10:20 +0800

    ✨ updated postTweet button to now log a string on click;

--- src/app/create-tweet/create-tweet.component.spec.ts
+++ src/app/create-tweet/create-tweet.component.spec.ts
@@ -31,4 +31,35 @@ describe('CreateTweetComponent', () => {
     );
     expect(buttonElement).toBeTruthy();
   });
+
+  it('should log a string on click of powetTweet button', () => {
+    const compiled = TestBed.createComponent(CreateTweetComponent)
+      .nativeElement as HTMLElement;
+    const buttonElement = compiled.querySelector(
+      'button[data-test="postTweet"]'
+    ) as HTMLButtonElement;
+    const logSpy = spyOn(console, 'log');
+    buttonElement.click();
+    expect(logSpy).toHaveBeenCalledWith(jasmine.stringContaining('tweet: '));
+  });
+
+  // it('should log the inputted tweet on click of postTweet button', () => {
+  //   // arrange
+  //   const compiled = TestBed.createComponent(CreateTweetComponent)
+  //     .nativeElement as HTMLElement;
+  //   const tweetInput = compiled.querySelector(
+  //     'input[data-test="tweet"]'
+  //   ) as HTMLInputElement;
+  //   const INPUT_TWEET = 'hello achi';
+  //   tweetInput.value = INPUT_TWEET;
+  //   tweetInput.dispatchEvent(new Event('input'));
+  //   const buttonElement = compiled.querySelector(
+  //     'button[data-test="postTweet"]'
+  //   ) as HTMLButtonElement;
+  //   const logSpy = spyOn(console, 'log');
+  //   // act
+  //   buttonElement.click();
+  //   // assert
+  //   expect(logSpy).toHaveBeenCalledWith(INPUT_TWEET);
+  // });
 });
