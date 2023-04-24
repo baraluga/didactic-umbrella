fabd07a0e5b998f9ccdae693b45cb7d0820325b2
Brian Alexander <brian.peralta@engie.com>
2023-04-24 14:51:55 +0800

    ✨ added way to input a tweet in create-tweet.component

--- src/app/create-tweet/create-tweet.component.spec.ts
+++ src/app/create-tweet/create-tweet.component.spec.ts
@@ -8,16 +8,18 @@ describe('CreateTweetComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateTweetComponent ]
-    })
-    .compileComponents();
+      declarations: [CreateTweetComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CreateTweetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show an input for tweet', () => {
+    const compiled = TestBed.createComponent(CreateTweetComponent)
+      .nativeElement as HTMLElement;
+    const inputElement = compiled.querySelector('input[data-test="tweet"]');
+    expect(inputElement).toBeTruthy();
   });
 });
