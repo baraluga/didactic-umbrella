14aee154524f663e031efa1186517e9100abc30d
Brian Alexander <brian.peralta@engie.com>
2023-04-24 16:03:58 +0800

    integrated create-tweet module to app.module;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateTweetModule } from './create-tweet/create-tweet.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CreateTweetModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
