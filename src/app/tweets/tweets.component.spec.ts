ef05605fd68410754a9316f02c9cd1a4c78193ec
Brian Alexander <brian.peralta@engie.com>
2023-04-25 11:51:04 +0800

    integrated getTweets() to tweets.component

--- src/app/tweets/tweets.component.spec.ts
+++ src/app/tweets/tweets.component.spec.ts
@@ -1,6 +1,7 @@
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
 import { TweetsComponent } from './tweets.component';
 import { TweetsModule } from './tweets.module';
+import { TweetManagerService } from '../services';
 
 describe('TweetsComponent', () => {
   const create = createComponentFactory({
@@ -11,10 +12,21 @@ describe('TweetsComponent', () => {
   let spec: Spectator<TweetsComponent>;
 
   beforeEach(() => {
-    spec = create();
+    spec = create({ detectChanges: false });
   });
 
-  it('should be ok', () => {
-    expect(spec).toBeTruthy();
+  it('should show a list', () => {
+    expect(spec.query('ul[data-test="tweets"]')).toExist();
+  });
+
+  it('should show the tweets from service', () => {
+    spyOn(spec.inject(TweetManagerService), 'getTweets').and.returnValue([
+      'manam',
+      'love it',
+    ]);
+    spec.detectChanges();
+    const elIce = spec.queryAll('li');
+    expect(elIce).toHaveLength(2);
+    expect(elIce[1]).toHaveText('love it');
   });
 });
--- src/app/tweets/tweets.component.ts
+++ src/app/tweets/tweets.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TweetManagerService } from '../services';
 
 @Component({
   selector: 'app-tweets',
   templateUrl: './tweets.component.html',
   styleUrls: ['./tweets.component.scss'],
 })
-export class TweetsComponent {}
+export class TweetsComponent implements OnInit {
+  tweets: string[] = [];
+  constructor(private readonly manager: TweetManagerService) {}
+
+  ngOnInit(): void {
+    this.tweets = this.manager.getTweets();
+  }
+}
