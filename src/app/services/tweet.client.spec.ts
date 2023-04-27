1f3ff37c8a3ce289e1185d772252aa9960df2ed9
Brian Alexander <brian.peralta@engie.com>
2023-04-27 12:18:40 +0800

    added getAll() to tweet.client;

--- src/app/services/tweet.client.spec.ts
+++ src/app/services/tweet.client.spec.ts
@@ -1,15 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
 import { TweetClient } from './tweet.client';
 
 describe('TweetClientService', () => {
-  const create = createServiceFactory({ service: TweetClient });
+  const create = createServiceFactory({
+    service: TweetClient,
+    mocks: [HttpClient],
+  });
   let spec: SpectatorService<TweetClient>;
 
   beforeEach(() => {
     spec = create();
   });
 
-  it('should be ok', () => {
-    expect(spec).toBeTruthy();
+  describe('when getting all tweets...', () => {
+    it('should make a GET request from the correct endpoint', () => {
+      const getFn = spec.inject(HttpClient).get;
+      spec.service.getAll();
+      expect(getFn).toHaveBeenCalledWith(jasmine.stringContaining('tweets'));
+    });
   });
 });
--- src/app/services/tweet.client.ts
+++ src/app/services/tweet.client.ts
@@ -1,6 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TweetClient {}
+export class TweetClient {
+  constructor(private readonly http: HttpClient) {}
+
+  getAll(): void {
+    this.http.get(this.buildTweetsEndpoint());
+  }
+
+  private buildTweetsEndpoint(): string {
+    return `${this.buildBaseUrl()}/tweets`;
+  }
+
+  private buildBaseUrl(): string {
+    return 'http://localhost:3000';
+  }
+}
